94f1dd7e47a261414149fdf66b83b9e9ef1cdad2
yano01 <yano_y01@pci-sol.com>
2020-06-24 17:08:30 +0900

    test

--- securing-web/src/main/java/com/example/securingweb/MvcConfig.java
+++ securing-web/src/main/java/com/example/securingweb/MvcConfig.java
@@ -1,5 +1,6 @@
 package com.example.securingweb;
 
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
